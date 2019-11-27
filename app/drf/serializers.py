a9d0496da6266088147246e4b3f0335d0ec6ddeb
geekwise-estefania-flores <geekwise.estefania.flores@gmail.com>
2019-11-27 18:57:12 +0000

    created serializer file with user and group classes

---
+++ app/drf/serializers.py
@@ -0,0 +1,20 @@
+from django.contrib.auth.models import User, Group
+from rest_framework import serializers 
+	
+    class User_Serilizers( serializers.HyperlinkedModelSerializer ):
+		class Meta:
+			model = User 
+			fields = [
+				'Url',
+                'Username',
+                'Email'
+                'Groups',
+            ]
+
+    class Group_Serilializer(serializers.HyperlinkedModelSerialzer ):   
+	    class Meta:
+		    model = Group 
+		    fields = [
+                'Username',
+                'Email'
+            ]
