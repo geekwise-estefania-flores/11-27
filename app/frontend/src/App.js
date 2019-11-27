53e513f9cfcdecfc313ddad1bdaa99c12918f6f7
geekwise-estefania-flores <geekwise.estefania.flores@gmail.com>
2019-11-27 23:10:28 +0000

    installed axios

--- app/frontend/src/App.js
+++ app/frontend/src/App.js
@@ -1,6 +1,6 @@
-// frontend/src/App.js
+import React, { Component } from "react";
+    import Modal from "./components/Modal";
 
-    import React, { Component } from "react";
     const todoItems = [
       {
         id: 1,
@@ -31,10 +31,33 @@
       constructor(props) {
         super(props);
         this.state = {
+          modal: false,
           viewCompleted: false,
+          activeItem: {
+            title: "",
+            description: "",
+            completed: false
+          },
           todoList: todoItems
         };
       }
+      toggle = () => {
+        this.setState({ modal: !this.state.modal });
+      };
+      handleSubmit = item => {
+        this.toggle();
+        alert("save" + JSON.stringify(item));
+      };
+      handleDelete = item => {
+        alert("delete" + JSON.stringify(item));
+      };
+      createItem = () => {
+        const item = { title: "", description: "", completed: false };
+        this.setState({ activeItem: item, modal: !this.state.modal });
+      };
+      editItem = item => {
+        this.setState({ activeItem: item, modal: !this.state.modal });
+      };
       displayCompleted = status => {
         if (status) {
           return this.setState({ viewCompleted: true });
@@ -78,8 +101,18 @@
               {item.title}
             </span>
             <span>
-              <button className="btn btn-secondary mr-2"> Edit </button>
-              <button className="btn btn-danger">Delete </button>
+              <button
+                onClick={() => this.editItem(item)}
+                className="btn btn-secondary mr-2"
+              >
+                Edit
+              </button>
+              <button
+                onClick={() => this.handleDelete(item)}
+                className="btn btn-danger"
+              >
+                Delete
+              </button>
             </span>
           </li>
         ));
@@ -92,7 +125,9 @@
               <div className="col-md-6 col-sm-10 mx-auto p-0">
                 <div className="card p-3">
                   <div className="">
-                    <button className="btn btn-primary">Add task</button>
+                    <button onClick={this.createItem} className="btn btn-primary">
+                      Add task
+                    </button>
                   </div>
                   {this.renderTabList()}
                   <ul className="list-group list-group-flush">
@@ -101,6 +136,13 @@
                 </div>
               </div>
             </div>
+            {this.state.modal ? (
+              <Modal
+                activeItem={this.state.activeItem}
+                toggle={this.toggle}
+                onSave={this.handleSubmit}
+              />
+            ) : null}
           </main>
         );
       }
